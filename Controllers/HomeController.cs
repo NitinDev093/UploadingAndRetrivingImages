c64e67b11816b131af42c7fc536e96399ef8b030
NitinDev093 <nitinsahani093@gmail.com>
2025-09-23 21:19:59 +0530

    added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -123,10 +123,14 @@ namespace Razer_View.Controllers
         }
 
 
-
+        //API calling view
+        public ActionResult CreateEmployee()
+        {
+            return View();
+        }
         //API calling
         [HttpPost]
-        public async Task<ActionResult> Create(Employee employee)
+        public async Task<ActionResult> Create(EmployeeModel employee)
         {
             using (var client = new HttpClient())
             {
@@ -153,19 +157,4 @@ namespace Razer_View.Controllers
 
 
     }
-
-    public class Employee
-    {
-        public int EmployeeID { get; set; } // Not required for insert
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
-        public string PhoneNumber { get; set; }
-        public DateTime HireDate { get; set; }
-        public string JobTitle { get; set; }
-        public int? DepartmentID { get; set; }
-        public decimal Salary { get; set; }
-        public int? ManagerID { get; set; }
-    }
-
 }
---
+++ Models/EmployeeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Razer_View.Models
+{
+    public class EmployeeModel
+    {
+        public int EmployeeID { get; set; } // Not required for insert
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; }
+        public DateTime HireDate { get; set; }
+        public string JobTitle { get; set; }
+        public int? DepartmentID { get; set; }
+        public decimal Salary { get; set; }
+        public int? ManagerID { get; set; }
+    }
+}
